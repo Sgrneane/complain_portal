513a47182c1e019d70e34ebefb10f69eef6f44ba
Sgrneane <sgrneaneprof@gmail.com>
2023-12-19 16:34:51 +0545

    message

--- management/serializer.py
+++ management/serializer.py
@@ -4,10 +4,6 @@ class ComplainSerializer(serializers.ModelSerializer):
     complain_status = serializers.SerializerMethodField()
     complain_priority = serializers.SerializerMethodField()
     complain_category=serializers.SerializerMethodField()
-    complain_sub_category=serializers.SerializerMethodField()
-    fullname=serializers.SerializerMethodField()
-    phone_number=serializers.SerializerMethodField()
-    address=serializers.SerializerMethodField()
     class Meta:
         model=Complain
         fields= '__all__'
@@ -17,26 +13,21 @@ class ComplainSerializer(serializers.ModelSerializer):
     def get_complain_priority(self, obj):
         return obj.get_priority()
     def get_complain_category(self,obj):
-        return obj.complain_category.category_name
-    def get_complain_sub_category(self,obj):
-        return obj.complain_sub_category.name
+        return obj.broad_category.english_name
     
-    def is_registered(self,obj):
+    def to_representation(self,instance):
+        obj = instance 
+        representation = super().to_representation(instance)
         if obj.created_by==None:
-            return False
+            representation['fullname']=(obj.is_anonymous.first_name + " " + obj.is_anonymous.last_name)
+            representation['pnone_number']=obj.is_anonymous.phone_number
+            representation['address']=(obj.is_anonymous.address)
         else:
-            return True
-    if is_registered==True:
-        def get_fullname(self,obj):
-            return (obj.created_by.first_name+" "+obj.created_by.last_name)
-        def get_phone_number(self,obj):
-            return obj.created_by.phone_number
-        def get_address(self,obj):
-            return obj.created_by.address
-    else:
-        def get_fullname(self,obj):
-            return (obj.is_anonymous.first_name+" "+obj.is_anonymous.last_name)
-        def get_phone_number(self,obj):
-            return (obj.is_anonymous.phone_number)
-        def get_address(self,obj):
-            return obj.is_anonymous.address
+            representation['fullname']=(obj.created_by.first_name + " " + obj.created_by.last_name)
+            representation['pnone_number']=obj.created_by.phone_number
+            representation['address']=(obj.created_by.address)
+        return representation
+
+
+
+
