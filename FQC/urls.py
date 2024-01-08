dfb3b295dd8c269be8c1eb9523b78bd2cb37dadf
Sgrneane <sgrneaneprof@gmail.com>
2024-01-08 17:17:59 +0545

    Debug to true

--- FQC/urls.py
+++ FQC/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('nepalmap/',include('nepalmap.urls')),
     path('social-auth/',include('social_django.urls', namespace='social')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
